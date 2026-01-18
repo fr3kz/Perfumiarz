b1d6d68251c62d2463e3e4a10fd14025fabc9f7d
fr3kz <tektmap1@gmail.com>
2026-01-18 18:44:56 +0100

    Develop

--- PerfumeService/Controllers/HomeController.cs
+++ PerfumeService/Controllers/HomeController.cs
@@ -6,7 +6,44 @@ namespace PerfumeService.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
     public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+    
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
+    public IActionResult Blog()
+    {
+        return View();
+    }
+    
+    public IActionResult Contact()
     {
         return View();
     }
